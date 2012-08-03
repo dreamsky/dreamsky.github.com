97c08e16c224cc101b3efe76a7b83e5af4ef9bd9
Hongbo Li <lhb25@163.com>
2012-08-03 16:46:25 +0800

    update

---
+++ tools/code/js/template.js
@@ -0,0 +1,17 @@
+var HTMLiveCodeTemplate = '<!DOCTYPE html>\n\
+<html>\n\
+	<head>\n\
+		<meta charset="utf-8" \/>\n\
+		<title>Demo<\/title>\n\
+		<style type="text\/css">\n\
+			\n\
+		<\/style>\n\
+		<script src="js\/jquery-1.7.1.min.js"><\/script>\n\
+		<script type="text\/javascript">\n\
+			\n\
+		<\/script>\n\
+	<\/head>\n\n\
+	<body>\n\
+		\n\
+	<\/body>\n\
+<\/html>';
